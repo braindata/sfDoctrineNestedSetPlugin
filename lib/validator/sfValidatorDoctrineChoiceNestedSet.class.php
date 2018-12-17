ac425c3767eb244917c6b01072ff97237399a864
Johannes Tyra (BD) <johannes.tyra@braindata.de>
2018-12-17 12:07:57 +0100

    CHANGE: Code Migration // Changed call Doctrine:: => Doctrine_Core::

--- lib/validator/sfValidatorDoctrineChoiceNestedSet.class.php
+++ lib/validator/sfValidatorDoctrineChoiceNestedSet.class.php
@@ -37,7 +37,7 @@ class sfValidatorDoctrineChoiceNestedSet extends sfValidatorBase
     }
     else
     {
-      $targetNode = Doctrine::getTable($this->getOption('model'))->find($value)->getNode();
+      $targetNode = Doctrine_Core::getTable($this->getOption('model'))->find($value)->getNode();
       if ($targetNode->isDescendantOfOrEqualTo($this->getOption('node')))
       {
         throw new sfValidatorError($this, 'node', array('value' => $value));
